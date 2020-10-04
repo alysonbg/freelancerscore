65839b9349e11181606d7b35ee703dcd08679c1c
Alyson Girao <alysongirao@gmail.com>
2020-10-04 15:21:22 -0300

    Added project configuration

---
+++ freelancerscore/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for freelancerscore project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelancerscore.settings')
+
+application = get_wsgi_application()
