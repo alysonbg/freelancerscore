2d722fba436a2697ad735a223c7c39253380b2d7
Alyson Girao <alysongirao@gmail.com>
2020-10-11 19:24:50 -0300

    Created /freelancers endpoint

---
+++ freelancerscore/api/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from freelancerscore.api.models import Skill, Experience, Freelancer
+
+
+admin.site.register(Skill)
+admin.site.register(Experience)
+admin.site.register(Freelancer)
