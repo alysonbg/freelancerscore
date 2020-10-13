64fee1c7e5d17c3e191ed6c1b9a0a2e43d589f36
Alyson Girao <alysongirao@gmail.com>
2020-10-12 22:07:50 -0300

    Added endpoint to return freelancer's computedscore

--- freelancerscore/api/views.py
+++ freelancerscore/api/views.py
@@ -1,8 +1,26 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from freelancerscore.api.serializers import FreelancerSerializer
 from freelancerscore.api.models import Freelancer
+from freelancerscore.api import business_logic
+from rest_framework import status
 
 
 class ListFreelancer(generics.ListAPIView):
     queryset = Freelancer.objects.all()
     serializer_class = FreelancerSerializer
+
+
+class FreelancerScore(APIView):
+    def get(self, request, *args, **kwargs):
+        freelancer = Freelancer.objects.get(pk=kwargs.get('pk'))
+        freelancer_computed_skills = business_logic.compute_score(freelancer)
+        data = {
+            'freelancer': {
+                'id': freelancer.id,
+                'computedSkills': freelancer_computed_skills
+            }
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
