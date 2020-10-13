f86ed8ec3b0b9e9eaf03167e8fa5a70b8f996286
Alyson Girao <alysongirao@gmail.com>
2020-10-12 22:07:50 -0300

    Added endpoint to return freelancer's computedscore

--- freelancerscore/api/urls.py
+++ freelancerscore/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from freelancerscore.api.views import ListFreelancer
+from freelancerscore.api.views import FreelancerScore, ListFreelancer
 
 
 urlpatterns = [
     path('freelancers/', ListFreelancer.as_view(), name='list-freelancers'),
+    path('freelancers/<int:pk>/score/', FreelancerScore.as_view(), name='freelancer-score')
 ]
